16b94f28f494eae4e2508b8e168dc311740dfa0f
JLangley753 <42213489+JLangley753@users.noreply.github.com>
2018-09-11 09:54:25 +0100

    Added spinner wheel with basic styling
    Deleted default react content, created and styled spinner wheel with segments for each spinner result. Button to run spinner function created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,13 +6,30 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">What do you want to play?</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hello world</p>
+        <div className="Spinner-body">
+          <div className="Spinner-wheel">
+            <div className = "Spinner-outer-segment g1" id="game-1">
+              <div className = "Spinner-inner-segment">Rainbow Six Siege</div>
+            </div>
+            <div className = "Spinner-outer-segment g2" id="game-2">
+              <div className = "Spinner-inner-segment">Rocket League</div>
+            </div>
+            <div className = "Spinner-outer-segment g3" id="game-3">
+              <div className = "Spinner-inner-segment">Jackbox.tv</div>
+            </div>
+            <div className = "Spinner-outer-segment g4" id="game-4">
+              <div className = "Spinner-inner-segment">Golf With Your Friends</div>
+            </div>
+            <div className = "Spinner-dial" id="Spinner-dial">
+            Spin
+            </div>
+          </div>
+          <div className="Spinner-button">
+            <button>Click me</button>
+          </div>
+        </div>
       </div>
     );
   }
