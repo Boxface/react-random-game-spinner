0151c5f9ce6dd2322548fcaad9ab924e60a2128b
JLangley753 <42213489+JLangley753@users.noreply.github.com>
2018-09-11 11:19:39 +0100

    Added basic functionality to spinner wheel
    Made adjustments to wheel wrapping, added JS to spin 'spin dial' section.

--- src/App.js
+++ src/App.js
@@ -9,29 +9,37 @@ class App extends Component {
           <h1 className="App-title">What do you want to play?</h1>
         </header>
         <div className="Spinner-body">
-          <div className="Spinner-wheel">
-            <div className = "Spinner-outer-segment g1" id="game-1">
-              <div className = "Spinner-inner-segment">Rainbow Six Siege</div>
+          <div className="Spinner-outer-wheel">
+            <div className="Spinner-inner-wheel" id="spinnerWheel">
+                <div className = "Spinner-outer-segment g1" id="game-1">
+                  <div className = "Spinner-inner-segment">Rainbow Six Siege</div>
+                </div>
+                <div className = "Spinner-outer-segment g2" id="game-2">
+                  <div className = "Spinner-inner-segment">Rocket League</div>
+                </div>
+                <div className = "Spinner-outer-segment g3" id="game-3">
+                  <div className = "Spinner-inner-segment">Jackbox.tv</div>
+                </div>
+                <div className = "Spinner-outer-segment g4" id="game-4">
+                  <div className = "Spinner-inner-segment">Golf With Your Friends</div>
+                </div>
             </div>
-            <div className = "Spinner-outer-segment g2" id="game-2">
-              <div className = "Spinner-inner-segment">Rocket League</div>
-            </div>
-            <div className = "Spinner-outer-segment g3" id="game-3">
-              <div className = "Spinner-inner-segment">Jackbox.tv</div>
-            </div>
-            <div className = "Spinner-outer-segment g4" id="game-4">
-              <div className = "Spinner-inner-segment">Golf With Your Friends</div>
-            </div>
-            <div className = "Spinner-dial" id="Spinner-dial">
-            Spin
-            </div>
+            <div className = "Spinner-dial" id="Spinner-dial">Spin</div>
           </div>
-          <div className="Spinner-button">
-            <button>Click me</button>
-          </div>
+        </div>
+        <div className="Spinner-button">
+        <button onClick={spin}>Click me</button>
         </div>
       </div>
     );
+    function spin() {
+      var totalDegrees = 1800;
+      var spinDegrees = Math.floor()
+
+      var wheel = document.getElementById('Spinner-dial');
+      wheel.style.transform = 'rotate(900deg)';
+      console.log('All working ok');
+    };
   }
 }
 
